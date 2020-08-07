1642ca8458e7be05313088124b219a7790fe7c0f
Luiz Taira <luiztaira@MacBook-Pro-de-Luiz-2.local>
2020-08-06 22:54:38 -0300

    primeiro commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CovidInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
